38ea5e975883335a6a6fc7ebd8eb7de11510d532
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2013-11-05 11:11:16 -0500

    Added column marker to markdown mode

--- elisp/mode-customizations.el
+++ elisp/mode-customizations.el
@@ -2,6 +2,9 @@
 (add-hook 'scheme-mode-hook (lambda () (column-marker-1 102)))
 (add-hook 'scheme-mode-hook (lambda () (setq-default fill-column 102)))
 
+;; Show a column marker in markdown mode
+(add-hook 'markdown-mode-hook (lambda () (column-marker-1 80)))
+
 ;; Paredit for all S-expression-based programming modes
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'scheme-mode-hook 'paredit-mode)
