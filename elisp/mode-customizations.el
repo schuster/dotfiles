b8b11ac2404a091beea3145d61b8b523d2c2b742
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2014-01-21 17:09:03 -0500

    Fix emacsclient for magit

--- elisp/mode-customizations.el
+++ elisp/mode-customizations.el
@@ -8,3 +8,8 @@
 ;; Paredit for all S-expression-based programming modes
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'scheme-mode-hook 'paredit-mode)
+
+;; Fix magit commit message editor, as seen here: http://stackoverflow.com/a/19265280/21957
+(if (equal 'darwin system-type)
+    (set-variable 'magit-emacsclient-executable "/usr/local/bin/emacsclient"))
+
