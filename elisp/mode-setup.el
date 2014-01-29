efdc3eac9e1e18a532876430de36b8799e0c490f
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2014-01-29 15:30:02 -0500

    Add Cisco mode

--- elisp/mode-setup.el
+++ elisp/mode-setup.el
@@ -1,12 +1,14 @@
 ;; Quack mode for Racket
 (require 'quack)
 
+(require 'fractL)
+
 ;; ParEdit
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
 
 ;; File extensions/major mode bindings
 (add-to-list 'auto-mode-alist '("\\.rkt$" . scheme-mode))
-(add-to-list 'auto-mode-alist '("\\.transit$" . scheme-mode))
+(add-to-list 'auto-mode-alist '("\\.transit$" . fractL-mode))
 (add-to-list 'auto-mode-alist '("\\.ttest$" . scheme-mode))
 (add-to-list 'auto-mode-alist '("\\.k$" . k3-mode))
