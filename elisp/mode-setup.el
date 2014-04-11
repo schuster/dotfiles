2b689c7438c5cbfde480182f9514e1019fc7a849
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2014-04-11 09:32:51 -0400

    Add hook for racket-mode for .ls files

--- elisp/mode-setup.el
+++ elisp/mode-setup.el
@@ -7,5 +7,6 @@
 ;; File extensions/major mode bindings
 (add-to-list 'auto-mode-alist '("\\.rkt$" . racket-mode))
 (add-to-list 'auto-mode-alist '("\\.transit$" . fractL-mode))
+(add-to-list 'auto-mode-alist '("\\.ls$" . racket-mode))
 (add-to-list 'auto-mode-alist '("\\.ttest$" . racket-mode))
 (add-to-list 'auto-mode-alist '("\\.k$" . k3-mode))
