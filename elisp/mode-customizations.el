2526ccd412198d6dffc07113949c7e707aa6f4ec
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2014-07-28 09:57:32 -0400

    Add BUG as a highlightable comment keyword

--- elisp/mode-customizations.el
+++ elisp/mode-customizations.el
@@ -30,7 +30,7 @@
 
 This functions should be added to the hooks of major modes for programming."
   (font-lock-add-keywords
-   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):"
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\|BUG\\):"
           1 font-lock-warning-face t))))
 
 (add-hook 'prog-mode-hook 'font-lock-comment-annotations)
