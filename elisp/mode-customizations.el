be5afecbfd306f83ef74a80bf72f5ba843e12fdb
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2014-11-20 11:02:58 -0500

    Add NOTE to highlighted keywords

--- elisp/mode-customizations.el
+++ elisp/mode-customizations.el
@@ -31,7 +31,7 @@
 
 This functions should be added to the hooks of major modes for programming."
   (font-lock-add-keywords
-   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\|BUG\\):"
+   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|NOTE\\|OPTIMIZE\\|HACK\\|REFACTOR\\|BUG\\):"
           1 font-lock-warning-face t))))
 
 (add-hook 'prog-mode-hook 'font-lock-comment-annotations)
