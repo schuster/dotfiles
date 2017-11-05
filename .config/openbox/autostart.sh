50647d989e937c618d3acf9d510cf5ce126081fd
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2017-11-05 10:34:25 -0500

    Add Openbox configuration

---
+++ .config/openbox/autostart.sh
@@ -0,0 +1,25 @@
+#
+# These things are run when an Openbox X Session is started.
+# You may place a similar script in $HOME/.config/openbox/autostart
+# to run user-specific things.
+#
+
+# If you want to use GNOME config tools...
+#
+#if test -x /usr/lib/openbox/gnome-settings-daemon >/dev/null; then
+#  /usr/lib/openbox/gnome-settings-daemon &
+#elif which gnome-settings-daemon >/dev/null 2>&1; then
+#  gnome-settings-daemon &
+#fi
+
+# If you want to use XFCE config tools...
+#
+#xfce-mcs-manager &
+
+# set the wallpaper
+feh --bg-fill --no-fehbg ~/images/backgrounds/DrumsofND.jpg
+
+# D-bus
+if which dbus-launch >/dev/null 2>&1 && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+       eval `dbus-launch --sh-syntax --exit-with-session`
+fi
