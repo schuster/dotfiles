4b53e9c8fd03747526f6835b2110401efc512758
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2019-06-23 10:58:01 -0400

    Remove old reference

--- .emacs.d/elisp/mode-setup.el
+++ .emacs.d/elisp/mode-setup.el
@@ -1,12 +1,9 @@
-(require 'fractL)
-
 ;; ParEdit
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
 
 ;; File extensions/major mode bindings
 (add-to-list 'auto-mode-alist '("\\.rkt$" . racket-mode))
-(add-to-list 'auto-mode-alist '("\\.transit$" . fractL-mode))
 (add-to-list 'auto-mode-alist '("\\.ls$" . racket-mode))
 (add-to-list 'auto-mode-alist '("\\.ttest$" . racket-mode))
 (add-to-list 'auto-mode-alist '("\\.k$" . k3-mode))
