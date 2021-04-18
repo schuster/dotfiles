5e8c51cbdaa092f8e718d476166f5690186a6e05
Jonathan Schuster <jonathan.p.schuster@gmail.com>
2021-04-18 13:15:26 -0400

    Update emacs java preferences

--- .emacs.d/elisp/mode-customizations.el
+++ .emacs.d/elisp/mode-customizations.el
@@ -16,7 +16,12 @@
 (add-hook 'racket-mode-hook 'paredit-mode)
 
 ;; Indentation for Fundamentals 2 at Northeastern
-(add-hook 'java-mode-hook (lambda () (setq tab-width 2) (setq c-basic-offset 2)))
+(add-hook 'java-mode-hook
+          (lambda ()
+            (setq tab-width 2)
+            (setq c-basic-offset 2)
+            (setq show-trailing-whitespace nil)
+            (column-marker-1 100)))
 
 ;; Custom indentations for racket-mode
 (put 'ls-test-case 'racket-indent-function 1)
